1650b4e3407cbd4729ec8cb227d2b139c187708c
er tiruzz <tirino.christian@iisgalvanimi.edu.it>
2024-03-18 12:57:24 +0000

    non ricordo se ho cambiato qualcosa

--- Esercizio6/src/app/lista-giocatori/lista-giocatori.component.ts
+++ Esercizio6/src/app/lista-giocatori/lista-giocatori.component.ts
@@ -10,15 +10,18 @@ import { Component } from '@angular/core';
 })
 export class ListaGiocatoriComponent {
   persone = [
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper", pagina: "https://liquipedia.net/counterstrike/S1mple"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper", pagina: "https://liquipedia.net/counterstrike/B1t"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper", pagina: "https://liquipedia.net/counterstrike/W0nderful"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper", pagina: "https://liquipedia.net/counterstrike/B1ad3"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper", pagina: "https://liquipedia.net/counterstrike/Aleksib"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper", pagina: ""},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper", pagina: ""},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper", pagina: ""},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper", pagina: ""},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper", pagina: ""},
   ];
+  openExternalPage(url: string) {
+    window.open(url, "_blank");
+  }
 }
