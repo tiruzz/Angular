bf92f651a214d5c89a4aad0860537d9ae1cd6a46
er tiruzz <tirino.christian@iisgalvanimi.edu.it>
2024-03-18 11:04:24 +0000

    progressi fighi

--- Esercizio6/src/app/app.component.ts
+++ Esercizio6/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListaGiocatoriComponent } from './lista-giocatori/lista-giocatori.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListaGiocatoriComponent],
+  imports: [RouterOutlet, ListaGiocatoriComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Esercizio6/src/app/lista-giocatori/lista-giocatori.component.ts
+++ Esercizio6/src/app/lista-giocatori/lista-giocatori.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'lista-giocatori',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lista-giocatori.component.html',
   styleUrl: './lista-giocatori.component.css'
 })
 export class ListaGiocatoriComponent {
-
+  persone = [
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
+    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nUtente: "tiruz", eta: "55", nazionalita: "italiano", ruolo: "sniper"},
+  ];
 }
