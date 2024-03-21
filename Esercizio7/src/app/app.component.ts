27b4d9f7b2183b65c08e4d3eb76fc5ca2d988513
er tiruzz <tirino.christian@iisgalvanimi.edu.it>
2024-03-21 13:13:37 +0000

    primi passi

---
+++ Esercizio7/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PokemonComponent } from './pokemon/pokemon.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, PokemonComponent,],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Esercizio7';
+}
