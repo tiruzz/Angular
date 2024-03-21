0dfaebc6908b057e237e05b8bef12ebc41d6ffd4
er tiruzz <tirino.christian@iisgalvanimi.edu.it>
2024-03-21 22:26:08 +0000

    messo il santo border color

--- Esercizio7/src/app/pokemon/pokemon.component.ts
+++ Esercizio7/src/app/pokemon/pokemon.component.ts
@@ -12,9 +12,9 @@ export class PokemonComponent {
   mostra: number = 0; 
   pokemon =[
     //prima generazione
-    {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", nome:"Bulbasaur",numero:"0001", colore: "verde", altezza:"0,7m ", categoria:"Seme", abilita:"Erbaiuto", peso:"6.9kg"},
-    {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", nome:"Charmander",numero:"0004", colore: "rosso", altezza:"0,6 m", categoria:"Lucertola", abilita:"Aiutofuoco", peso:"8,5 kg"},
-    {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png", nome:"Squirtle",numero:"0007", colore: "blu", altezza:"0,5 m", categoria:"Tartaghina", abilita:"Acquaiuto", peso:"9,0 kg"},
+    {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", nome:"Bulbasaur", numero:"0001", colore: "verde", altezza:"0,7m ", categoria:"Seme", abilita:"Erbaiuto", peso:"6.9kg"},
+    {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", nome:"Charmander", numero:"0004", colore: "rosso", altezza:"0,6 m", categoria:"Lucertola", abilita:"Aiutofuoco", peso:"8,5 kg"},
+    {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png", nome:"Squirtle", numero:"0007", colore: "blu", altezza:"0,5 m", categoria:"Tartaghina", abilita:"Acquaiuto", peso:"9,0 kg"},
     //seconda generazione
     {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/152.png", nome:"Chikorita",numero:"0152", colore: "verde", altezza:"0,9 m", categoria:"Foglia", abilita:"Erbaiuto", peso:"6,4 kg"},
     {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/155.png", nome:"Cyndaquil",numero:"0155", colore: "rosso", altezza:"0,5 m", categoria:"Fuocotopo", abilita:"Aiutofuoco", peso:"7,9 kg"},
