9db02df869538cb5605a22ddf9f7bc2aa46c924d
er tiruzz <tirino.christian@iisgalvanimi.edu.it>
2024-03-21 13:43:53 +0000

    forse primo commit

--- Esercizio7/src/app/pokemon/pokemon.component.ts
+++ Esercizio7/src/app/pokemon/pokemon.component.ts
@@ -9,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './pokemon.component.css'
 })
 export class PokemonComponent {
-  
+  pokemon =[
+    //prima generazione
+    {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", nome:"Bulbasaur",numero:"0001", altezza:"0,7m ", categoria:"Seme", abilita:"Erbaiuto", peso:"6.9kg"},
+    {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", nome:"Charmander",numero:"0004", altezza:"0,6 m", categoria:"Lucertola", abilita:"Aiutofuoco", peso:"8,5 kg"},
+    {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png", nome:"Squirtle",numero:"0007", altezza:"0,5 m", categoria:"Tartaghina", abilita:"Acquaiuto", peso:"9,0 kg"},
+    //seconda generazione
+    {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/152.png", nome:"Chikorita",numero:"0152", altezza:"0,9 m", categoria:"Foglia", abilita:"Erbaiuto", peso:"6,4 kg"},
+    {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/155.png", nome:"Cyndaquil",numero:"0155", altezza:"0,5 m", categoria:"Fuocotopo", abilita:"Aiutofuoco", peso:"7,9 kg"},
+    {immagine:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/158.png", nome:"Totodile",numero:"0158", altezza:"0,6 m", categoria:"Mascellone", abilita:"Acquaiuto", peso:"9,5 kg"},
+  ]
 }
